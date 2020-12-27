18c823fcf9edff75cca1d927342da73491c0dd13
Ihor Chikh <Spiningistigor@gmail.com>
2020-12-27 23:34:33 +0200

    add role managment

--- WebApplication/WebApplication/Controllers/HomeController.cs
+++ WebApplication/WebApplication/Controllers/HomeController.cs
@@ -3,12 +3,14 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using WebApplication.Models;
 
 namespace WebApplication.Controllers
 {
+    [Authorize(Roles="admin")]
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
