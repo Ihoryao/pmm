ef1b4192f4db186342ae4f3eef9c1f17f1f32861
Ihor Chikh <Spiningistigor@gmail.com>
2021-01-10 14:32:40 +0200

    add feature

---
+++ WebAPIApplication/WebAPIApplication/Models/Sage.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+
+namespace WebAPIApplication.Models
+{
+    public class Sage
+    {
+        public int SageId { get; set; }
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public string Photo { get; set; }
+        public string City { get; set; }
+
+        public virtual ICollection<Book> Books { get; set; }
+    }
+}
