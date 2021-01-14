39fd7e85f791b3c4dbd67fe03ad167a563fa29a1
Ihor Chikh <Spiningistigor@gmail.com>
2021-01-14 20:01:37 +0200

    unitOfWork

---
+++ WebAPIApplication/WebAPIApplication/DAL/SageBookRepository.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+using WebAPIApplication.Models;
+
+namespace WebAPIApplication.DAL
+{
+    public class SageBookRepository : IRepository<Book>
+    {
+        private SageBookContext db;
+ 
+        public SageBookRepository(SageBookContext context)
+        {
+            this.db = context;
+        }
+ 
+        public IEnumerable<Book> GetAll()
+        {
+            return db.Books;
+        }
+ 
+        public Book Get(int id)
+        {
+            return db.Books.Find(id);
+        }
+ 
+        public void Create(Book book)
+        {
+            db.Books.Add(book);
+        }
+ 
+        public void Update(Book book)
+        {
+            db.Entry(book).State = EntityState.Modified;
+        }
+ 
+        public void Delete(int id)
+        {
+            Book book = db.Books.Find(id);
+            if (book != null)
+                db.Books.Remove(book);
+        }
+    }
+}
---
+++ WebAPIApplication/WebAPIApplication/DAL/UnitOfWork.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using WebAPIApplication.Models;
+
+namespace WebAPIApplication.DAL
+{
+    public class UnitOfWork// : IDisposable
+    {
+    //     private SageBookContext db = new SageBookContext();
+    //     private SageBookRepository bookRepository;
+    //     //private OrderRepository orderRepository;
+    //
+    //     // public BookRepository Books
+    //     // {
+    //     //     get
+    //     //     {
+    //     //         if (bookRepository == null)
+    //     //             bookRepository = new BookRepository(db);
+    //     //         return bookRepository;
+    //     //     }
+    //     // }
+    //     //
+    //     // public OrderRepository Orders
+    //     // {
+    //     //     get
+    //     //     {
+    //     //         if (orderRepository == null)
+    //     //             orderRepository = new OrderRepository(db);
+    //     //         return orderRepository;
+    //     //     }
+    //     // }
+    //     //
+    //     public void Save()
+    //     {
+    //         db.SaveChanges();
+    //     }
+    //
+    //     private bool disposed = false;
+    //
+    //     public virtual void Dispose(bool disposing)
+    //     {
+    //         if (!this.disposed)
+    //         {
+    //             if (disposing)
+    //             {
+    //                 db.Dispose();
+    //             }
+    //             this.disposed = true;
+    //         }
+    //     }
+    //
+    //     public void Dispose()
+    //     {
+    //         Dispose(true);
+    //         GC.SuppressFinalize(this);
+    //     }
+     }
+}
