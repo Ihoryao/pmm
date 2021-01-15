380d43645d3e607103d29215fffcbc5fb0d87359
Ihor Chikh <Spiningistigor@gmail.com>
2021-01-15 15:07:45 +0200

    identity repo added

---
+++ WebAPIApplication/WebAPIApplication/DAL/IdentityRepository.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+using WebAPIApplication.Models;
+
+namespace WebAPIApplication.DAL
+{
+    public class IdentityRepository : IRepository<User>
+    {
+        private AuthContext _context;
+
+        public IdentityRepository(AuthContext context)
+        {
+            this._context = context;
+        }
+
+        public IEnumerable<User> GetAll()
+        {
+            return _context.Users;
+        }
+
+        public User Get(int id)
+        {
+            return _context.Users.Find(id);
+        }
+
+        public void Create(User user)
+        {
+            _context.Users.Add(user);
+        }
+
+        public void Update(User user)
+        {
+            _context.Entry(user).State = EntityState.Modified;
+        }
+
+        public void Delete(int id)
+        {
+            User user = _context.Users.Find(id);
+            if (user != null)
+                _context.Users.Remove(user);
+        }
+    }
+}
